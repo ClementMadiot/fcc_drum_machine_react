63d7cbf2534655324ed38714894691186aa669c1
ClementMadiot <clementmadiot09@gmail.com>
2024-10-08 10:56:59 +0200

    add array drum pad

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import './style/App.scss'
+import audioData from "./data/drumAudio";
+import "./style/App.scss";
 
 function App() {
-
-
   return (
-    <>
-    
-    </>
-  )
+    <main>
+      <div id="drum-machine">
+        {audioData.map((item) => (
+          <div className="drum-pad" id={item.id}>
+            <audio className="clip" id={item.letter} src={item.song}></audio>
+            {item.letter}
+          </div>
+        ))}
+      </div>
+      <div id="pad">
+        <div className="logo">FCC&nbsp;
+          <i class="inner-logo fa fa-free-code-camp"></i>
+        </div>
+        <div id="display">&nbsp;</div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/data/drumAudio.js
@@ -0,0 +1,49 @@
+const audioData = [
+  {
+    id: "heater1",
+    letter: "Q",
+    song: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3"
+  },
+  {
+    id: "heater2",
+    letter: "W",
+    song: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3"
+  },
+  {
+    id: "heater3",
+    letter: "E",
+    song: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3"
+  },
+  {
+    id: "heater4",
+    letter: "A",
+    song: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3"
+  },
+  {
+    id: "clap",
+    letter: "S",
+    song: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3"
+  },
+  {
+    id: "openHH",
+    letter: "D",
+    song: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3"
+  },
+  {
+    id: "kickHat",
+    letter: "Z",
+    song: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3"
+  },
+  {
+    id: "kick",
+    letter: "X",
+    song: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3"
+  },
+  {
+    id: "closedHH",
+    letter: "C",
+    song: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3"
+  }
+];
+
+export default audioData;
